48ab2c1e5260d7e36b8291927a3a31d99beadcae
Dracgamer5643 <supermee5643@gmail.com>
2026-01-05 19:17:54 +0530

    Updated the logic for version controll.

--- im_cost_model_app/app/page.js
+++ im_cost_model_app/app/page.js
@@ -369,7 +369,7 @@ export default function Page() {
   ============================ */
   return (
     <div>
-      <p className='w-full bg-red-200 text-black text-sm p-2'>{updateVersionMessage}</p>
+      <p className='w-full bg-red-200 text-black text-sm py-2 px-4 font-semibold'>{updateVersionMessage}</p>
       <div className="px-4 print:hidden">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center px-4 py-2 bg-white shadow rounded gap-2 md:gap-0">
           <SkuDescription isLoading={isLoading} allFormData={allFormData} setAllFormData={setAllFormData} sheetName={sheetName} setCountryName={setCountryName} />
