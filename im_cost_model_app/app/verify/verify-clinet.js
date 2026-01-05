1c62b5e5d43d3dd70e3f2726108dd8bbfcb6f227
Dracgamer5643 <supermee5643@gmail.com>
2026-01-05 17:11:54 +0530

    Updated the errors in a toast so users get to know the issue.

--- im_cost_model_app/app/verify/verify-clinet.js
+++ im_cost_model_app/app/verify/verify-clinet.js
@@ -41,6 +41,12 @@ export default function VerifyClient() {
                     email: res.email,
                 });
 
+                if(!api.success) {
+                    toast.error(api.message  || "Server Issue Sheet Loading Failed");
+                    setStatus("error");
+                    return;
+                }
+
                 setStatus("success");
 
                 setTimeout(() => router.push("/"), 1500);
